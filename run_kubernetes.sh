a02bb62769bfff8c5dcb8353a96e18b9f84320c8
Victoria Hanna Eva Riess <58560391+VictoriaRiess@users.noreply.github.com>
2020-07-07 22:07:14 +0200

    Update run_kubernetes.sh
    making better

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,17 +5,15 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-dockerpath=victoriariess/microsvrcapi
+dockerpath="victoriariess/microsvrcapi"
 tag=v0.1
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run make_prediction \
-    --generator=run-pod/v1 \
-    --image=$dockerpath:$tag 
-    --port=80 \
-    --labels app=microsvrcapi
-  
+kubectl run microsvrcapi \
+            --image=$dockerpath:$tag \
+            --port=80 
+            --labels app=microsvrcapi
 
 # Step 3:
 # List kubernetes pods
@@ -23,5 +21,4 @@ kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-kubectl port-forward make_prediction 80:80
-
+kubectl port-forward microsvrcapi 8000:80
