366c41ea96c776f0b4f848f2d2a5ed720f582f2f
Ubuntu <ubuntu@ip-172-31-75-123.us-west-2.compute.internal>
2020-07-08 13:45:41 +0000

    stripped off commnted lines

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -10,10 +10,7 @@ tag=v0.1
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run microsvrcapi \
-            --image=$dockerpath:$tag \
-            --port=80 \
-            --labels app=microsvrcapi
+kubectl run microsvrcapi --image=$dockerpath:$tag --port=80
 
 # Step 3:
 # List kubernetes pods
